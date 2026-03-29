4ac9fa7940ba449e9a47c531a32d3092041a9da2
qwertymundane <qwertymundane@gmail.com>
2026-03-29 19:56:05 +0530

    Create adv_coding_easy.py
    TCS Advanced Coding - Easy (2026 Mar 26): Added a new Python file with a program outline to find the maximum product of subsets from an array of integers.

---
+++ adv_coding_easy.py
@@ -0,0 +1,13 @@
+# Suppose an array of integers is given as the input to a program.
+# NOTE: The integers can be (-)ve, 0 or (+)ve.
+# Find subset of the array (solution subset can be of length 1 or the array length itself)
+# such that the product of all the elements is the maximum product obtained 
+# when compared to the product obtained by all the possible subsets of the array.
+# INPUT: array [1<=len(array)<2^40] -- e.g (input): -1 2 3 4 -- read as `str` not `int`
+# OUTPUT: the maximum product obtained from all possible subsets of the input array elements
+
+# function max_sub_prod()
+
+# function len_sub_prod()
+
+# main program
