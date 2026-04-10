4b14f95caf72a5e7b335e730625d3b33e25aaa85
qwertymundane <qwertymundane@gmail.com>
2026-04-10 19:58:47 +0530

    Refine comments in adv_coding_easy.py
    Updated comments for clarity and precision regarding input and subset length.

--- adv_coding_easy.py
+++ adv_coding_easy.py
@@ -1,9 +1,9 @@
 # Suppose an array of integers is given as the input to a program.
 # NOTE: The integers can be (-)ve, 0 or (+)ve.
-# Find subset of the array (solution subset can be of length 1 or the array length itself)
+# Find subset of the array (solution subset can be of length 1 or the entire array length itself)
 # such that the product of all the elements is the maximum product obtained 
 # when compared to the product obtained by all the possible subsets of the array.
-# INPUT: array [1<=len(array)<2^40] -- e.g (input): -1 2 3 4 -- read as `str` not `int`
+# INPUT: array [1<=len(array)<2^40] -- e.g (input): -1 2 3 4 -- read as `str` dtype not `int` dtype
 # OUTPUT: the maximum product obtained from all possible subsets of the input array elements
 
 # function max_sub_prod()
